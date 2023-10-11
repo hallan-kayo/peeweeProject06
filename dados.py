38057b357d6e2ec827dfc9d86c5c8c58bf1c6c64
Halan Caio <hallankayo20@gmail.com>
2023-10-11 19:00:22 -0300

    inserção e update de dados completo

--- dados.py
+++ dados.py
@@ -65,7 +65,7 @@ notebook = Produtos(
 
 mesa = Produtos(
     descricao = 'Mesa', 
-    categoria = Categoria.select().where(Categoria.delete == 'Móveis'), 
+    categoria = Categoria.select().where(Categoria.descricao == 'Móveis'), 
     valor = 549.95
 )
 # mesa.save()
@@ -83,6 +83,7 @@ ar = Produtos(
     valor = 2999
 )
 # ar.save()
+
 caderno = Produtos(
     descricao = 'Caderno', 
     categoria = Categoria.select().where(Categoria.descricao == 'Papelaria'),
@@ -104,35 +105,47 @@ borracha = Produtos(
 )
 # borracha.save()
 
-# def preencher_venda(nome_cliente, nome_produto, quantidade,data):
-#     id_cliente = Cliente.select(Cliente.id).where(Cliente.nome == nome_cliente)
-#     id_produto = Produtos.select(Produtos.id).where(Produtos.descricao == nome_produto)
-#     valor_produto = Produtos.select(Produtos.valor).where(Produtos.id == id_produto)
-#     data_venda = datetime(data)
-#     info_venda = Vendas(id_produto,id_cliente,data_venda,quantidade,valor_produto,(valor_produto*quantidade))
-#     return info_venda
+def cadastrar_venda(nome_cliente, nome_produto, quantidade_itens, data_venda):
+    venda = Vendas(
+        cliente = Cliente.select().where(Cliente.nome == nome_cliente),
+        produto = Produtos.select().where(Produtos.descricao == nome_produto),
+        quantidade = quantidade_itens,
+        data = data_venda,
+        valor_unitario = Produtos.select(Produtos.valor).where(
+            Produtos.id == Produtos.select().where(Produtos.descricao == nome_produto)),
+        valor_total = (Produtos.select().where(
+            Produtos.id == Produtos.select().where(
+                Produtos.descricao == 'Notebook').get()).get().valor*quantidade_itens)
+    )
+    venda.save()
 
-# def cadastrar_venda(venda):
-#     venda = preencher_venda(venda.nome_cliente, venda.nome_produto, venda.quantidade)
-#     venda.save()
 
-# venda1 = Vendas('João da Silva', 'Caneta Bic esferográfica azul', 5, datetime(2023,6,17,10,20))
-# # venda1.save()
-# venda2 = Vendas('João da Silva', 'Notebook', 1, datetime(2023,6,17,10,20))
-# # venda2.save()
-# venda3 = Vendas('Carlos Santos', 'Caderno', 2, datetime(2021,10,7,15,10))
-# # venda3.save()
-# venda4 = Vendas('Ana Oliveira', 'Ar Condicionado', 3, datetime(2022,9,17,12,20))
-# # venda4.save()
-# venda5 = Vendas('Pedro Pereira', 'Notebook', 10, datetime(2022,9,11,9,27))
-# # venda5.save()
-# venda6 = Vendas('Maria de Souza', 'mesa', 5, datetime(2023,6,1,17,20))
-# # venda6.save()
-# venda7 = Vendas('Ana Oliveira', 'Borracha', 100, datetime(2022,9,3,7,0))
-# # venda7.save()
-# venda8 = Vendas('Ana Oliviera', 'Livro', 2, datetime(2022,9,3,7,0))
-# # venda8.save()
-# venda9 = Vendas('Carlos Santos', 'Ar Condicionado', 15, datetime(2022,9,12,16,50))
-# # venda9.save()
-# venda10 = Vendas('Maria de Souza', 'Caneta Bic esferográfica azul', 10, datetime(2023,4,17,10,20))
-# # venda10.save()
+# cadastrar_venda('João da Silva', 'Caneta Bic esferográfica azul', 5, datetime(2023,6,17,10,20))
+# cadastrar_venda('João da Silva', 'Notebook', 1, datetime(2023,6,17,10,20))
+# cadastrar_venda('Carlos Santos', 'Caderno', 2, datetime(2021,10,7,15,10))
+# cadastrar_venda('Ana Oliveira', 'Ar Condicionado', 3, datetime(2022,9,17,12,20))
+# cadastrar_venda('Pedro Pereira', 'Notebook', 10, datetime(2022,9,11,9,27))
+# cadastrar_venda('Maria Souza', 'Mesa', 5, datetime(2023,6,1,17,20))
+# cadastrar_venda('Ana Oliveira', 'Borracha', 100, datetime(2022,9,3,7,0))
+# cadastrar_venda('Ana Oliveira', 'Livro', 2, datetime(2022,9,3,7,0))
+# cadastrar_venda('Carlos Santos', 'Ar Condicionado', 15, datetime(2022,9,12,16,50))
+# cadastrar_venda('Maria Souza', 'Caneta Bic esferográfica azul', 10, datetime(2023,4,17,10,20))
+
+# ----------------------------------------------------------------------------------------------------------
+
+# q1 = Produtos.select(Produtos.valor).where(Produtos.descricao == "Notebook").get()
+# print(q1.valor)
+
+# q1 = Produtos.select().where(
+#     Produtos.id == Produtos.select().where(Produtos.descricao == 'Notebook').get()).get().valor
+q2 = Produtos.select(Produtos.id).where(Produtos.descricao == 'Caneta Bic esferográfica azul').get().id
+# print(q1)
+# print(q2)
+
+update1=Produtos.update({Produtos.valor:4}).where(Produtos.id == Produtos.select(Produtos.id).where(
+    Produtos.descricao == 'Caneta Bic esferográfica azul'))
+# update1.execute()
+
+update2=Produtos.update({Produtos.valor:2}).where(Produtos.id == Produtos.select(Produtos.id).where(
+    Produtos.descricao == 'Caneta Bic esferográfica azul'))
+# update2.execute()
